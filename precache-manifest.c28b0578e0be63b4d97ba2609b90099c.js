7bcbc530fa309a089e2621217122a97fd313c462
ryan-rayark <48552331+ryan-rayark@users.noreply.github.com>
2019-04-04 20:25:51 +0530

    final

---
+++ precache-manifest.c28b0578e0be63b4d97ba2609b90099c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "e792c46c7eb009d3e1c4",
+    "url": "/robofriends/static/js/main.1ee5c831.chunk.js"
+  },
+  {
+    "revision": "dc194aa61ccbd134b988",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "e792c46c7eb009d3e1c4",
+    "url": "/robofriends/static/css/main.5ef6b3c8.chunk.css"
+  },
+  {
+    "revision": "dc194aa61ccbd134b988",
+    "url": "/robofriends/static/css/2.6d29704c.chunk.css"
+  },
+  {
+    "revision": "5cbdd6390740d702f99f7f54b5c4d46b",
+    "url": "/robofriends/index.html"
+  }
+];
